1acd10f74dcf953fbba2d1b22c41c3268308eaf4
ValfridoNovais <146680175+ValfridoNovais@users.noreply.github.com>
2024-07-27 05:46:44 -0300

    Alteração de Função pra Cçasse no Models

--- setup/urls.py
+++ setup/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
-from todos.views import todo_list
+#from todos.views import todo_list
+from todos.views import TodoListView
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", todo_list),
+    path("", TodoListView.as_view()),
+    path("create", TodoCreatView.as_view())
+    #path("", todo_list),
 ]
